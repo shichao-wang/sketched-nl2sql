c0a54ead4ee8ed2c0ef5e9950e1f28a5985b8189
Shichao Wang <shichao.w@outlook.com>
2020-04-04 16:22:28 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 requirements = open("requirements.txt").readlines()
 setup(
     name="sketched_nl2sql",
-    pack_dir={"": "src"},
+    package_dir={"": "src"},
     packages=find_packages("src"),
     install_requires=requirements,
     zip_safe=False,
